7707c54b1d8e2ba4ab1d2b4cb1e74bbdf1dcd267
davilucas <91565261+Davilucca22@users.noreply.github.com>
2023-11-25 17:47:58 -0300

    conta no banco

--- senha do banco.py
+++ senha do banco.py
@@ -1,8 +1,12 @@
-padrao = (1234)
-for c in range (0,3):
-    senha = int (input('digite sua senha para ver saldo em conta\n:'))
-    if senha != padrao :
-        print('senha invalida!')
-if senha == padrao:
-    print('saldo disponivel:1500.00')
-        
+cont = 0
+while True:
+    usuario = str(input('nome de usuario:')).lower().strip()
+    senha = int (input('senha:').strip())
+    if usuario == 'davi' and senha == 1234:
+        print('saldo:1.500')
+        break
+    if usuario != 'davi' or senha != 1234:
+        print('senha ou usuario invalidos!')
+        cont = cont + 1
+    if cont >= 3 :
+        print('conta bloqueada')
