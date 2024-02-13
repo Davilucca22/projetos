74513ad639783625bb97c6e3bd9dc755e85ba6bb
davilucas <91565261+Davilucca22@users.noreply.github.com>
2024-02-12 21:58:33 -0300

    V 1.0.1

--- gerador_de_senhas.py
+++ gerador_de_senhas.py
@@ -10,6 +10,11 @@ simbolos = '@#$%&*'
 
 num = int(input('quantos digitos sua senha deve ter?:'))
 
+print('-'*30)
+print('sua senha:',end='')
 for c in range (0,num):
     senha = random.choice((minusculas)+(maiusculas)+(numeros)+(simbolos))
     print(senha,end='')
+
+print()
+print('-'*30)
